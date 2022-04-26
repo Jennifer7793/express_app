dd7d31b2dfcfd0115b40319e2775ce12bcc7e4c8
JenJen <jennifer7793@gmail.com>
2022-04-26 13:27:41 +0800

    post method to create user

--- server.js
+++ server.js
@@ -55,3 +55,35 @@ app.get("/api/user/:id", (req, res, next) => {
     })
   });
 });
+
+app.post("/api/user/", (req, res, next) => {
+  var errors = []
+  if (!req.body.password) {
+    errors.push("No password specified");
+  }
+  if (!req.body.email) {
+    errors.push("No email specified");
+  }
+  if (errors.length) {
+    res.status(400).json({"error": errors.join(",")});
+    return;
+  }
+  var data = {
+    name: req.body.name,
+    email: req.body.email, 
+    password: md5(req.body.password)
+  }
+  var sql = 'INSERT INTO user(name, email, password) VALUES(?, ?, ?)'
+  var params = [data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    }
+    res.json({
+      "message": "success", 
+      "data": data,
+      "id": this.lastID
+    })
+  })
+})
