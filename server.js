1ee6c38a3b180731c0591da7d9288e07f451a0a9
JenJen <jennifer7793@gmail.com>
2022-04-26 13:14:08 +0800

    get a single user by id

--- server.js
+++ server.js
@@ -35,3 +35,18 @@ app.get("/api/users", (req, res, next) => {
     })
   });
 });
+
+app.get("/api/user/:id", (req, res, next) => {
+  var sql = "select * from user where id = ?"
+  var params = [req.params.id]
+  db.get(sql, params, (err, row) => {
+    if(err) {
+      res.status(400).json({"error": err.message});
+      return;
+    }
+    res.json({
+      "message":"success", 
+      "data":row
+    })
+  });
+});
